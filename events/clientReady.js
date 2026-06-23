bdb0b538e56f0e26c0619c0db8e1b6245a7c189a
forhs <xkagias11@gmail.com>
2026-06-23 16:19:52 +0300

    track clientReady.js and move bot presence text to env vars

---
+++ events/clientReady.js
@@ -0,0 +1,30 @@
+const { ActivityType } = require('discord.js');
+const GiveawaySchema = require('../models/GiveawaySchema');
+const { endGiveaway } = require('../slashCommands/utility/giveaway');
+
+module.exports = {
+    name: 'clientReady',
+    once: true,
+    async execute(client) {
+        console.log(`Logged in as ${client.user.tag}`);
+
+        const activityName = process.env.BOT_ACTIVITY_NAME;
+        if (activityName) {
+            const activityType = ActivityType[process.env.BOT_ACTIVITY_TYPE] ?? ActivityType.Watching;
+            client.user.setPresence({
+                activities: [{ name: activityName, type: activityType }],
+            });
+        }
+
+        const active = await GiveawaySchema.find({ ended: false });
+        for (const giveaway of active) {
+            const remaining = giveaway.endsAt.getTime() - Date.now();
+            if (remaining <= 0) {
+                await endGiveaway(client, giveaway);
+            } else {
+                setTimeout(() => endGiveaway(client, giveaway), remaining);
+            }
+        }
+        if (active.length) console.log(`[giveaway] Restored ${active.length} active giveaway(s).`);
+    }
+};
